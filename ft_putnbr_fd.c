c74a9a4e5f4753fa424920aaf5524e6faebc0eff
Pavel Guranda <pguranda@2-F-5.42heilbronn.de>
2022-04-25 16:58:26 +0200

    Final version

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pguranda <pguranda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/12 11:45:26 by pguranda          #+#    #+#             */
+/*   Updated: 2022/04/19 15:34:45 by pguranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#define LONG_MIN -2147483648
+#define LONG_MAX 2147483647
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n > LONG_MAX || n < LONG_MIN)
+		return ;
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		n = n * -1;
+		ft_putchar_fd('-', fd);
+	}
+	if (n < 10)
+	{
+		ft_putchar_fd(n + '0', fd);
+		return ;
+	}
+	ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd((n % 10) + '0', fd);
+	return ;
+}
